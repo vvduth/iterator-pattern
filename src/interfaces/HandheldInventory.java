d9adf85dd7eea0ed1709c9b364638d0a94dcbde7
Duc Thai <ducthai060501@gmail.com>
2022-10-22 14:54:11 +0300

    set up pattern

---
+++ src/interfaces/HandheldInventory.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class HandheldInventory implements Inventory {
+    // item right: public getter private setter
+    // item left. ssam with left
+    // constrctor handledItemInventory (rightHand, leftHand) this.right = rightHand, this.left = leftHand
+    /* 
+        public InventoryItemIterator getIterator() {
+            return new HandheldItemIterator(this) ;
+            // pass this instance
+        }
+    */ 
+}
---
+++ src/interfaces/Inventory.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface Inventory {
+    // getIterator
+}
